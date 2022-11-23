13eec5cc136cb91436583d46ba88be60efa2d8b3
radu781 <poparadu501@gmail.com>
2022-11-23 23:05:17 +0200

    Lab 6 done

--- lab6/exercices.py
+++ lab6/exercices.py
@@ -5,11 +5,7 @@ from bs4 import BeautifulSoup
 
 
 def ex1(text: str) -> list[str]:
-    return [
-        word
-        for word in re.split(r"\s+", text)
-        if re.search(r"[a-z\d]+", word, re.IGNORECASE)
-    ]
+    return re.findall(r"\b\w+[!?.]?", text)
 
 
 def ex2(regex: str, text: str, x: int) -> list[str]:
@@ -26,36 +22,60 @@ def ex2(regex: str, text: str, x: int) -> list[str]:
 
 
 def ex3(text: str, regexes: list[str]) -> list[str]:
-    return [str(re.findall(regex, text)) for regex in regexes]
+    return list(
+        map(
+            lambda x: x.group(0),  # type: ignore
+            filter(
+                lambda x: x is not None,
+                (re.match(regex, text) for regex in regexes),
+            ),
+        )
+    )
 
 
-def __has_all_tags(xml_soup: BeautifulSoup, attrs: dict[str, str]) -> bool:
-    for attr in attrs:
-        if not xml_soup.has_attr(attr):
-            return False
-        if xml_soup[attr] != attrs[attr]:
-            return False
-    return True
+# python < 3.11
+# def ex4(xml_path: str, attrs: dict[str, str]) -> list[str]:
+#     if not os.path.exists(xml_path):
+#         raise ValueError(f"Path {os.path.abspath(xml_path)} does not exist")
+
+#     out: list[str] = []
+#     with open(xml_path, "r") as file:
+#         soup = BeautifulSoup(file, "xml")
+#         current = list(soup.find_all(attrs=attrs))
+#         if current:
+#             out.append(current)  # type: ignore
+#     return out
 
 
-def ex4(xml_path: str, attrs: dict[str, str]) -> list[str]:
+def __contains_one_attr(xml_line: str, attr: tuple[str, str]) -> bool:
+    return re.search(rf'{attr[0]}\s*=\s*"{attr[1]}"(\s|>)', xml_line) is not None
+
+
+def ex4(xml_path: str, attrs: dict[str, str], attr_count=99) -> list[str]:
     if not os.path.exists(xml_path):
         raise ValueError(f"Path {os.path.abspath(xml_path)} does not exist")
 
     out: list[str] = []
     with open(xml_path, "r") as file:
-        soup = BeautifulSoup(file, "xml")
-        out = list(soup.find_all(attrs=attrs))
+        for line in file.readlines():
+            current_found = 0
+            for attr in attrs:
+                if __contains_one_attr(line, (attr, attrs[attr])):
+                    current_found += 1
+                    if current_found >= attr_count:
+                        out.append(line.rstrip("\n").strip())
+                        break
+
     return out
 
 
 def ex5(xml_path: str, attrs: dict[str, str]) -> list[str]:
-    ...
+    return ex4(xml_path, attrs, 1)
 
 
 def ex6(text: str) -> str:
-    matches = re.findall(r"([aeiou]\w*[aeiou])(?:(\s+|$))", text, re.IGNORECASE)
-    for word, _ in matches:
+    matches = re.findall(r"\b([aeiou]\w*[aeiou])\b", text, re.IGNORECASE)
+    for word in matches:
         current_word = ""
         for ele in word[::2]:
             current_word += ele + "*"
@@ -63,3 +83,40 @@ def ex6(text: str) -> str:
             current_word = current_word[:-1]
         text = text.replace(word, current_word)
     return text
+
+
+def ex7(cnp: str) -> bool:
+    if not (
+        re.search(
+            r"^[0-8]\d\d((0[1-9])|(1[0-2]))((0[1-9])|([12]\d)|(3[01]))\d{6}$", cnp
+        )
+    ):
+        return False
+    magic_sum = sum(
+        list(
+            int(zipped[0]) * int(zipped[1]) for zipped in zip("279146358279", cnp[:-1])
+        )
+    )
+    return int(cnp[-1]) == (magic_sum % 11 if magic_sum % 11 != 10 else 1)
+
+
+def ex8(dir_path: str, regex: str) -> list[str]:
+    if not os.path.exists(dir_path):
+        raise FileNotFoundError("Directory does not exist")
+    if not os.path.isdir(dir_path):
+        raise OSError(f"{dir_path} is not a directory")
+
+    out: list[str] = []
+    for dirpath, _, filenames in os.walk(dir_path):
+        for file in filenames:
+            if re.search(regex, file):
+                try:
+                    with open(f"{dirpath}/{file}", "r") as f:
+                        if re.search(regex, f.read()):
+                            out.append(f">>{file}")
+                except UnicodeDecodeError:
+                    pass
+            elif re.search(regex, file):
+                out.append(file)
+
+    return out
--- lab6/main.py
+++ lab6/main.py
@@ -5,12 +5,14 @@ def main():
     big_text = ""
     with open("lab6/lorem.txt", "r") as file:
         big_text = file.read()
-    # print(ex1(big_text))
-    # print(ex2(r"m$", big_text, 4))
-    # print(ex3(big_text, [".*", r"\w{7}"]))
-    # print(ex4("lab6/file.xml", {"k": "name", "v":"McDonald's"}))
-    # print(ex5("lab6/file.xml", {"k": "name", "v":"McDonald's"}))
-    print(ex6("aaaee tttet ae at ta aita"))
+    print(ex1(big_text))
+    print(ex2(r"m$", big_text, 4))
+    print(ex3(big_text, [".{8,15}", r"\w{7}"]))
+    print(ex4("lab6/file.xml", {"k": "name", "v": "McDonald's"}))
+    print(ex5("lab6/file.xml", {"k": "name", "v": "McDonald's"}))
+    print(ex6("aaaee tttet ae at    ta aita"))
+    print(ex7("1650606345845"))
+    print(ex8(".", "main"))
 
 
 if __name__ == "__main__":
